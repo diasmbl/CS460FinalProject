9b7c484dc4f38aaf5a31e4fab9b3799dbd3df9a2
diasmbl <155988823+diasmbl@users.noreply.github.com>
2025-02-24 18:21:46 -0700

    Initial Commit

---
+++ OneDrive/Desktop/CS460/banking-api/src/test/java/edu/ensign/cs460/banking_api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ensign.cs460.banking_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
